630ebc3422e2371cf8e04bdd7a178e2e8f9a55d1
nnair <nin500@g.harvard.edu>
2022-07-18 18:43:32 -0400

    login and create forms complete

--- app.py
+++ app.py
@@ -86,10 +86,32 @@ def video():
     
     return render_template('video.html')
 
-@app.route('/login')	
+
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    
-    return render_template('login.html')        
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+
+# Route for handling the create account page logic
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('create.html', error=error)
+
+
+
         
 # def detect_motion(frameCount):
 # 	# grab global references to the video stream, output frame, and
