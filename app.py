b588ac7d73e72c640952a3b1d0555c15ff08cbc5
lishaoyiharvard <shl183@g.harvard.edu>
2022-08-02 21:44:24 -0400

    add username to retrieve

--- app.py
+++ app.py
@@ -207,7 +207,7 @@ def user_retrieve(username, page):
                 title=f'@{profile_user.username}', pages=pages, limit=limit, route_name=f'/user/retrieve/{username}'
             )
         else:
-            return redirect( f'/user/retrieve/{pages}' )
+            return redirect( f'/user/retrieve/{username}/{pages}' )
 
     # Not logged in, go to login page
     except KeyError as ke:
